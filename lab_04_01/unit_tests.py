a2601d3b5cecf7316e7c9177ce4017b5ebbf7a57
taucuya <taisiyasht@gmail.com>
2024-04-28 15:54:59 +0300

    unit_tests added

--- lab_04_01/unit_tests.py
+++ lab_04_01/unit_tests.py
@@ -1,2 +1,21 @@
-def test():
-    pass
+import pytest
+from PyQt6.QtGui import QColor
+from PyQt6.QtCore import QPointF
+from mirror import mirror
+
+
+def test_mirror():
+    # Тест с отражением точки (0, 0) относительно центра (5, 5)
+    center_point = QPointF(0, 0)
+    cur_point = QPointF(5, 5)
+    expected_result = [
+        QPointF(5, 5), 
+        QPointF(-5, 5), 
+        QPointF(5, -5), 
+        QPointF(-5, -5), 
+        QPointF(5, 5), 
+        QPointF(-5, 5), 
+        QPointF(5, -5), 
+        QPointF(-5, -5)
+    ]
+    assert mirror([], cur_point, center_point) == expected_result
